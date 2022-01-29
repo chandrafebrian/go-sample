0324015352dec94e1950f26832400db238f0a1ad
chandrafebrian <chandrafebrian99@gmail.com>
2022-01-29 08:27:03 +0700

    baru
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
-	print("hello world")
+	var conferenceName = "Chandra "
+	const conferenceTiket = 50
+	var remainingTiket = 50
+
+	println("tiket yg tersedia tinggal", remainingTiket, "segera dapatkan")
+
+	println("welcome to to booking ticket", conferenceName)
+	println("dapatkan segera jumlah tiket saat ini hanya ada", conferenceTiket, "tunggu apa lagi")
+
 }
