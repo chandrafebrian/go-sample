bd2d0b71e5899bf8da45bbf452b2137cd231c13d
chandrafebrian <chandrafebrian99@gmail.com>
2022-01-29 13:20:20 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	// variable & conts
 	var conferenceName = "Chandra "
 	const conferenceTiket = 50
 	var remainingTiket = 50
-
+	//
 	println("tiket yg tersedia tinggal", remainingTiket, "segera dapatkan")
 
 	println("welcome to to booking ticket", conferenceName)
 	println("dapatkan segera jumlah tiket saat ini hanya ada", conferenceTiket, "tunggu apa lagi")
+
+	fmt.Printf("ini adalah kalimat %v thanks alot \n", conferenceName)
+
+	// type data
+	var userName string
+	var userTiket int
+
+	userName = "chandra"
+	userTiket = 90
+	fmt.Printf("ini adalah kalimat type data yg dibuat %v dengan nilai integer %v \n", userName, userTiket)
 
 }
